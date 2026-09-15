dd322d83d5bb10b32d3cd84722d0a672dca51f4b
balba <shazhuya@users.noreply.github.com>
2026-09-15 14:07:56 +0800

    feat(m1): export Sina provider

--- src/htcn/data/providers/__init__.py
+++ src/htcn/data/providers/__init__.py
@@ -1,5 +1,11 @@
 from .akshare_provider import AkShareProvider
+from .akshare_sina_provider import AkShareSinaProvider
 from .baostock_provider import BaoStockProvider
 from .failover import FailoverProvider
 
-__all__ = ["AkShareProvider", "BaoStockProvider", "FailoverProvider"]
+__all__ = [
+    "AkShareProvider",
+    "AkShareSinaProvider",
+    "BaoStockProvider",
+    "FailoverProvider",
+]
