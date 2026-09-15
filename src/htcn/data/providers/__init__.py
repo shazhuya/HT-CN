a11c4c3f60c25630968cca93133a7456728b36da
balba <shazhuya@users.noreply.github.com>
2026-09-15 14:00:38 +0800

    feat(m1): export failover provider

--- src/htcn/data/providers/__init__.py
+++ src/htcn/data/providers/__init__.py
@@ -1,4 +1,5 @@
 from .akshare_provider import AkShareProvider
 from .baostock_provider import BaoStockProvider
+from .failover import FailoverProvider
 
-__all__ = ["AkShareProvider", "BaoStockProvider"]
+__all__ = ["AkShareProvider", "BaoStockProvider", "FailoverProvider"]
